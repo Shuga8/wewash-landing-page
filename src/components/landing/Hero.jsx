4155ef9ba6f52c7219c32696c07693273fd48ddc
Shuga8 <lotocharles8@gmail.com>
2024-11-27 15:04:13 +0100

    heror text, styel

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -4,7 +4,18 @@ const Hero = () => {
   return (
     <>
       <div className="hero w-full h-[100dvh]">
-        <div className="content"></div>
+        <div className="content w-full h-full flex justify-center items-center flex-col gap-y-8 text-wrap px-12 base:px-32 xl:px-48 py-3">
+          <h1 className="title text-[2.2rem] base:text-[3rem] xl:text-[3.5rem] text-center">
+            Africa's pioneer in carwash service & systems
+          </h1>
+
+          <h2 className="sub-title text-center">
+            We are revolutionizing car wash services in Africa by introducing
+            innovative solutions to scale operations, ensure seamless and secure
+            transactions, and provide convenient, customer-focused real-life
+            experiences.
+          </h2>
+        </div>
       </div>
     </>
   );
