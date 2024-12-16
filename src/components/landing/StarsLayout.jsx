a1265444102bc26663acb2140773db1a5e58addb
Shuga8 <lotocharles8@gmail.com>
2024-12-16 14:28:03 +0100

    stars layout resize

--- src/components/landing/StarsLayout.jsx
+++ src/components/landing/StarsLayout.jsx
@@ -32,8 +32,23 @@ const StarsLayout = () => {
     }
     animate();
 
+    function handleWindowResize() {
+      const { width, height } = mapDiv.getBoundingClientRect();
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+      renderer.setSize(width, height);
+
+      if (window.matchMedia("(max-width: 600px)").matches) {
+        camera.position.z = 3.3;
+      } else {
+        camera.position.z = 2.5;
+      }
+    }
+    window.addEventListener("resize", handleWindowResize);
+
     return () => {
       mapDiv.removeChild(renderer.domElement);
+      window.removeEventListener("resize", handleWindowResize);
     };
   }, []);
 
